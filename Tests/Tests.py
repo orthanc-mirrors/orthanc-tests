bf62546e1e8345f1701d4385e036bc55c966a402
Sebastien Jodogne <s.jodogne@gmail.com>
2017-07-17 10:00:22 +0200

    also test GenericNoWildcardInDates

--- Tests/Tests.py
+++ Tests/Tests.py
@@ -1254,6 +1254,8 @@ class Orthanc(unittest.TestCase):
         DoPut(_REMOTE, '/modalities/tata', [ "STORESCP", "localhost", 2000, 'MedInria' ]) # check backward compatiblity with obsolete manufacturer
         DoDelete(_REMOTE, '/modalities/tata')
         DoPut(_REMOTE, '/modalities/tata', [ "STORESCP", "localhost", 2000, 'GenericNoWildcards' ])
+        DoDelete(_REMOTE, '/modalities/tata')
+        DoPut(_REMOTE, '/modalities/tata', [ "STORESCP", "localhost", 2000, 'GenericNoWildcardInDates' ])
         self.assertRaises(Exception, lambda: DoPut(_REMOTE, '/modalities/toto', [ "STORESCP", "localhost", 2000, 'InvalidManufacturerName' ]))
         self.assertTrue('store' in DoGet(_REMOTE, '/modalities/toto'))
         self.assertTrue('store' in DoGet(_REMOTE, '/modalities/tata'))
