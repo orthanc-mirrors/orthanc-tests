d6012bf12af6f4188189e740e8f97b7c1755320b
Alain Mazy <am@osimis.io>
2024-02-05 09:50:24 +0100

    rename PG downgrade script

--- NewTests/PostgresUpgrades/downgrade.sh
+++ NewTests/PostgresUpgrades/downgrade.sh
@@ -3,8 +3,8 @@
 pushd /scripts
 
 # TODO: change pg-transactions by the plugin version number !
-apt-get update && apt-get install -y wget && wget https://orthanc.uclouvain.be/hg/orthanc-databases/raw-file/pg-transactions/PostgreSQL/Plugins/SQL/Downgrades/V6.2ToV6.1.sql
-psql -U postgres -f V6.2ToV6.1.sql
+apt-get update && apt-get install -y wget && wget https://orthanc.uclouvain.be/hg/orthanc-databases/raw-file/tip/PostgreSQL/Plugins/SQL/Downgrades/Rev2ToRev1.sql
+psql -U postgres -f Rev2ToRev1.sql
 
 # if you want to test a downgrade procedure, you may use this code ...
 # psql -U postgres -f downgrade.sql
