6939c3af9ad7dd803c4f926b6bdfb9f690449819
Sebastien Jodogne <s.jodogne@gmail.com>
2024-04-02 18:44:09 +0200

    fix

--- Plugins/Worklists/Run.py
+++ Plugins/Worklists/Run.py
@@ -143,7 +143,7 @@ def RunQuery(source, ignoreTags):
                     if not tag in ignoreTags:
                         line_without_comment = re.sub(b'\s*#.*', b'', line)
                         line_without_comment = line_without_comment.replace(b'\0', b'')
-                        current.append(line_without_comment)
+                        current.append(line_without_comment[4:])
 
         if len(current) > 0:
             answers.append(current)
