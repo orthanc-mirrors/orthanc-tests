b23b703abd98f45636328bc126ad5e5af6c7cb89
Alain Mazy <am@orthanc.team>
2024-06-03 14:20:17 +0200

    fix path

--- NewTests/PostgresUpgrades/downgrade.sh
+++ NewTests/PostgresUpgrades/downgrade.sh
@@ -3,7 +3,7 @@
 pushd /scripts
 
 # TODO: change pg-transactions by the plugin version number !
-apt-get update && apt-get install -y wget && wget https://orthanc.uclouvain.be/hg/orthanc-databases/raw-file/tip/PostgreSQL/Plugins/SQL/Downgrades/Rev2ToRev1.sql
+apt-get update && apt-get install -y wget && wget https://orthanc.uclouvain.be/hg/orthanc-databases/raw-file/default/PostgreSQL/Plugins/SQL/Downgrades/Rev2ToRev1.sql
 psql -U postgres -f Rev2ToRev1.sql
 
 # if you want to test a downgrade procedure, you may use this code ...
