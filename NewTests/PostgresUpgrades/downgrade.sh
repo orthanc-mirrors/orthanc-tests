c6f61adff7dc10b2f5cfd655fba65679ad90406f
Alain Mazy <am@orthanc.team>
2024-11-15 09:39:01 +0100

    fix forbidden access to file on mercurial server

--- NewTests/PostgresUpgrades/downgrade.sh
+++ NewTests/PostgresUpgrades/downgrade.sh
@@ -2,9 +2,9 @@
 
 pushd /scripts
 
-# TODO: change pg-transactions by the plugin version number !
-apt-get update && apt-get install -y wget && wget https://orthanc.uclouvain.be/hg/orthanc-databases/raw-file/default/PostgreSQL/Plugins/SQL/Downgrades/Rev2ToRev1.sql
-psql -U postgres -f Rev2ToRev1.sql
+apt-get update && apt-get install -y wget mercurial
+hg clone https://orthanc.uclouvain.be/hg/orthanc-databases
+psql -U postgres -f /scripts/orthanc-databases/PostgreSQL/Plugins/SQL/Downgrades/Rev2ToRev1.sql
 
 # if you want to test a downgrade procedure, you may use this code ...
 # psql -U postgres -f downgrade.sql
