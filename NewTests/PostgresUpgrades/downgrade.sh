783a70e303651f18de3221f65d2a06c917dd7ff7
Alain Mazy <am@orthanc.team>
2024-11-27 09:30:47 +0100

    pg upgrade
    --HG--
    branch : find-refactoring

--- NewTests/PostgresUpgrades/downgrade.sh
+++ NewTests/PostgresUpgrades/downgrade.sh
@@ -4,7 +4,8 @@ pushd /scripts
 
 apt-get update && apt-get install -y wget mercurial
 hg clone https://orthanc.uclouvain.be/hg/orthanc-databases
-psql -U postgres -f /scripts/orthanc-databases/PostgreSQL/Plugins/SQL/Downgrades/Rev2ToRev1.sql
+hg update -r find-refactoring
+psql -U postgres -f /scripts/orthanc-databases/PostgreSQL/Plugins/SQL/Downgrades/Rev3ToRev2.sql
 
 # if you want to test a downgrade procedure, you may use this code ...
 # psql -U postgres -f downgrade.sql
