d57cc18f8ffcf223c2de8cb92ee7f374279932a2
Alain Mazy <am@orthanc.team>
2025-02-04 17:59:45 +0100

    fix
    --HG--
    branch : attach-custom-data

--- NewTests/PostgresUpgrades/downgrade.sh
+++ NewTests/PostgresUpgrades/downgrade.sh
@@ -4,11 +4,11 @@ pushd /scripts
 
 apt-get update && apt-get install -y wget mercurial
 hg clone https://orthanc.uclouvain.be/hg/orthanc-databases
-pushd orthanc-databases
 # TODO: change attach-custom-data by the plugin version number or "default" !
+pushd orthanc-databases
 hg update -r attach-custom-data
+
 psql -U postgres -f /scripts/orthanc-databases/PostgreSQL/Plugins/SQL/Downgrades/Rev4ToRev3.sql
-psql -U postgres -f /scripts/orthanc-databases/PostgreSQL/Plugins/SQL/Downgrades/Rev3ToRev2.sql
 
 # if you want to test a downgrade procedure, you may use this code ...
 # psql -U postgres -f downgrade.sql
