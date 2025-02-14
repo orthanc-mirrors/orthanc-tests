359c143821627c9b646c5f2b89112101b19a2948
Alain Mazy <am@orthanc.team>
2025-02-14 14:52:35 +0100

    pg-upgrade tests

--- NewTests/PostgresUpgrades/run-integ-tests-from-docker.sh
+++ NewTests/PostgresUpgrades/run-integ-tests-from-docker.sh
@@ -2,6 +2,6 @@
 
 set -ex
 
-/scripts/wait-for-it.sh orthanc-pg-15-6rev2-for-integ-tests:8042 -t 60
-# python /tests/orthanc-tests/Tests/Run.py --server=orthanc-pg-15-6rev2-for-integ-tests --force --docker -- -v  Orthanc.test_lua_deadlock
-python /tests/orthanc-tests/Tests/Run.py --server=orthanc-pg-15-6rev2-for-integ-tests --force --docker -- -v
+/scripts/wait-for-it.sh orthanc-pg-15-previous-revision-for-integ-tests:8042 -t 60
+# python /tests/orthanc-tests/Tests/Run.py --server=orthanc-pg-15-previous-revision-for-integ-tests --force --docker -- -v  Orthanc.test_lua_deadlock
+python /tests/orthanc-tests/Tests/Run.py --server=orthanc-pg-15-previous-revision-for-integ-tests --force --docker -- -v
