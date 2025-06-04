f414fad132c6302e92371fbdff13554d4d62ff55
Alain Mazy <am@orthanc.team>
2025-06-04 15:30:11 +0200

    pg upgrade tests
    --HG--
    branch : sql-opti

--- NewTests/PostgresUpgrades/downgrade.sh
+++ NewTests/PostgresUpgrades/downgrade.sh
@@ -5,7 +5,7 @@ pushd /scripts
 apt-get update && apt-get install -y wget mercurial
 hg clone https://orthanc.uclouvain.be/hg/orthanc-databases
 hg update -r default
-psql -U postgres -f /scripts/orthanc-databases/PostgreSQL/Plugins/SQL/Downgrades/Rev4ToRev3.sql
+psql -U postgres -f /scripts/orthanc-databases/PostgreSQL/Plugins/SQL/Downgrades/Rev99ToRev4.sql
 
 # if you want to test a downgrade procedure, you may use this code ...
 # psql -U postgres -f downgrade.sql
