dba338a7058003dd703b954a2478895c4ffe874c
Alain Mazy <am@orthanc.team>
2026-03-11 09:02:23 +0100

    download from http instead of hg clone that is often rejected from Azure runners

--- NewTests/PostgresUpgrades/downgrade.sh
+++ NewTests/PostgresUpgrades/downgrade.sh
@@ -2,10 +2,15 @@
 
 pushd /scripts
 
-apt-get update && apt-get install -y wget mercurial
-hg clone https://orthanc.uclouvain.be/hg/orthanc-databases
+apt-get update && apt-get install -y wget
+
+# hg clone is often rejected from Azure runners, let's try a download from http
+wget https://orthanc.uclouvain.be/downloads/sources/orthanc-postgresql/OrthancPostgreSQL-10.0.tar.gz --output-document /tmp/pg.tar.gz
+# ex for an intermediate release (or if download from uclouvain is rejected)
+# wget https://public-files.orthanc.team/tmp-builds/hg-repos/orthanc-databases-81d837d7d20d.tar.gz --output-document /tmp/pg.tar.gz
+
+tar xvf /tmp/pg.tar.gz --strip-components=1
 pushd /scripts/orthanc-databases/
-# hg update -r default
 
 psql -U postgres -f /scripts/orthanc-databases/PostgreSQL/Plugins/SQL/Downgrades/Rev10ToRev5.sql
 
